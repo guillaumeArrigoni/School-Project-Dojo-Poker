3b6fd4590afcdd0ced679521d485c35ee7fb229e
Loris Drid <loris.drid@etu.univ-cotedazur.fr>
2022-10-18 13:45:01 +0200

    S1 - Class Value

--- src/gamepoker/Value.java
+++ src/gamepoker/Value.java
@@ -1,6 +1,22 @@
 package gamepoker;
 
 public class Value {
-    public Value(String s, int i) {
+    private String str;
+    private int i;
+    public Value(String str, int i) {
+        this.str = str;
+        this.i = i;
+    }
+
+    public String getName() {
+        return str;
+    }
+
+    public int getValue() {
+        return i;
+    }
+
+    public boolean Equals(Value value1, Value value2) {
+        return (value1.getName() == value2.getName())&&(value1.getValue() == value2.getValue());
     }
 }
