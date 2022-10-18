5e92db75ef0d2c4df441ff0216c72777c20d7bbc
Loris Drid <loris.drid@etu.univ-cotedazur.fr>
2022-10-18 13:19:53 +0200

    S1 - Update Constructor of Value in ValueTest

--- Test/gamepoker/ValueTest.java
+++ Test/gamepoker/ValueTest.java
@@ -1,22 +1,32 @@
 package gamepoker;
 
-package gamepoker;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @org.junit.jupiter.api.Test
 
+
+
 public class ValueTest {
-    private Value value1 = new Value(8);
-    private Value value2 = new Value(3);
-    private Value value3 = new Value(14);
-    private Value value4 = new Value(13);
-    private Value value5 = new Value(12);
+    private Value value1 = new Value("8", 8);
+    private Value value2 = new Value("3", 3);
+    private Value value3 = new Value("A", 14);
+    private Value value4 = new Value("R", 13);
+    private Value value5 = new Value("10", 10);
 
     void getValueTest() {
 
-        assertEquals(value1.getValue(), 8  );
-        assertEquals(value4.getValue(), 13);
-        assertEquals(value2.getValue(), 3 );
-        assertEquals(value3.getValue(), 14);
-        assertEquals(value5.getValue(), 12);
+        assertEquals(8, value1.getValue());
+        assertEquals(3, value2.getValue());
+        assertEquals(14, value3.getValue());
+        assertEquals(13, value4.getValue());
+        assertEquals(10, value5.getValue());
+    }
+
+    void getNameTest() {
+        assertEquals("8", value1.getName());
+        assertEquals("3", value2.getName());
+        assertEquals("A", value3.getName());
+        assertEquals("R", value4.getName());
+        assertEquals("10", value5.getName());
     }
 }
