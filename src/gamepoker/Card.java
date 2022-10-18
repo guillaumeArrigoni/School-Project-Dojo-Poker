5b52b2ef38d07620a8556d53eb0ab7b007b9369e
Karim CHARLEUX <karim.charleux@etu.univ-cotedazur.fr>
2022-10-18 22:10:36 +0200

    S1 - Create Card class with JavaDoc

--- src/gamepoker/Card.java
+++ src/gamepoker/Card.java
@@ -1,18 +1,66 @@
 package gamepoker;
 
+/**
+ * The poker game contains 52 cards without joker, a card is composed by a value and a color
+ * @author Karim CHARLEUX & Yacine MERIOUA
+ */
 public class Card {
+    /** The value of the card with a position between 2 and 14 to determine the highest to the lowest card */
+    private Value value;
+
+    /**
+     * Create Card with a specified value
+     * @param value The value's card
+     */
     public Card(Value value) {
+        this.value = value;
     }
 
-    public short getValue() {
+    /**
+     * @return The value object of the current card
+     */
+    public Value getValue() {
+        return this.value;
     }
 
-    public double CompareHigherCard(Card c2) {
+    /**
+     * Allows to compare with another card on the current card the higher value
+     * @param card2 The second card
+     * @return False, if the second card is higher than current card or True if the second card is lower than current card
+     */
+    public boolean CompareHigherCard(Card card2) {
+        return this.value.getPosition() > card2.getValue().getPosition();
     }
 
-    public short getValue(Card c2) {
+    /**
+     * Overriding equals() to compare two Card objects
+     * @param object The Card object to compare to the current Card
+     * @return True if object is equals to current Card value or False if not
+     */
+    @Override
+    public boolean equals(Object object) {
+        // If the object is compared with itself then return true
+        if (object == this) {
+            return true;
+        }
+        // Check if object is an instance of Card or not
+        if (!(object instanceof Card)) {
+            return false;
+        }
+        // Typecast object to Card so that we can compare data
+        Card card2 = (Card) object;
+
+        // Compare the card value data and return accordingly
+        return card2.value.equals(this.value);
     }
 
-    public short getName(Card c2) {
+    /**
+     * Convert a card object in a string object to correct display
+     * @return the card converted to a string
+     */
+    @Override
+    public String toString() {
+        return this.value.toString();
     }
+
 }
