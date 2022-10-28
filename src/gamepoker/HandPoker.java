26ca610ffbf641232dc0c234ec857cf20ed41ee5
Karim CHARLEUX <karim.charleux@etu.univ-cotedazur.fr>
2022-10-28 12:40:17 +0200

    S2 - HandPoker Code optimization

--- src/gamepoker/HandPoker.java
+++ src/gamepoker/HandPoker.java
@@ -3,57 +3,57 @@ package gamepoker;
 import java.util.*;
 
 public class HandPoker {
-    private final ArrayList<Card> hand;
-    HashMap<Integer, Integer> handOccurrence ;
-    HashMap<Integer, ArrayList<Integer>> handCombination ;
 
-    public HandPoker(Card card1, Card card2, Card card3){
-        this.hand = new ArrayList<>();
-        this.hand.add(card1);
-        this.hand.add(card2);
-        this.hand.add(card3);
-        registerInDictionary(this.hand);
-        reverseDictionary(this.handOccurrence);
+    public static final int NBR_CARDS = 3;
+    private final ArrayList<Card> handCards;
+    private HashMap<Integer, Integer> handOccurrence;
+    private HashMap<Integer, ArrayList<Integer>> handCombination;
+
+    public HandPoker(Card card1, Card card2, Card card3) {
+        this.handCards = new ArrayList<>(Arrays.asList(card1, card2, card3));
+        this.handOccurrence = new HashMap<>();
+        this.handCombination = new HashMap<>();
+        registerInDictionary();
+        reverseDictionary();
     }
 
     /**
      * Associate to each different value of the card in a hand the number of card that share the same value
      */
-    private void registerInDictionary(ArrayList<Card> hand){
-        for (int i = 0;i<hand.size();i++){
-            if (handOccurrence.containsKey(hand.get(i).getValue().getPosition())) {
-                handOccurrence.replace(hand.get(i).getValue().getPosition(),handOccurrence.get(hand.get(i).getValue().getPosition())+1);
+    private void registerInDictionary() {
+        this.handCards.forEach((theCard -> {
+            if (this.handOccurrence.containsKey(theCard.getValue().getPosition())) {
+                this.handOccurrence.replace(theCard.getValue().getPosition(), this.handOccurrence.get(theCard.getValue().getPosition()) + 1);
             } else {
-                handOccurrence.put(hand.get(i).getValue().getPosition(),1);
+                this.handOccurrence.put(theCard.getValue().getPosition(), 1);
             }
-        }
+        }));
     }
 
     /**
      * Associate to each value of a dictionary all the key in an ArrayList
-     * @param dico : the dictionary we want to reverse
      */
-    private void reverseDictionary(HashMap<Integer,Integer> dico){
-        for (Map.Entry<Integer,Integer> entry : dico.entrySet()){
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
-            if (handCombination.containsKey(value)){
-                handCombination.get(value).add(key);
+    private void reverseDictionary() {
+        for (Map.Entry<Integer, Integer> entry : this.handOccurrence.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if (this.handCombination.containsKey(value)) {
+                this.handCombination.get(value).add(key);
             } else {
-                handCombination.put(value,new ArrayList<>(Collections.singletonList(key)));
+                this.handCombination.put(value, new ArrayList<>(Collections.singletonList(key)));
             }
         }
     }
 
-    public List<Card> getHand(){
-        return this.hand;
+    public List<Card> getHandCards() {
+        return this.handCards;
     }
 
-    public HashMap<Integer,Integer> getHandOccurrence(){
+    public Map<Integer, Integer> getHandOccurrence() {
         return this.handOccurrence;
     }
 
-    public HashMap<Integer,ArrayList<Integer>> getHandCombination(){
+    public Map<Integer, ArrayList<Integer>> getHandCombination() {
         return this.handCombination;
     }
 
@@ -63,6 +63,8 @@ public class HandPoker {
      */
     @Override
     public String toString() {
-        return (hand.get(0).toString() + " " + hand.get(1).toString());
+        String outputString = "";
+        this.handCards.forEach((card -> outputString.concat(card.toString() + " ")));
+        return outputString;
     }
 }
