bfb78a581463e38773cc9ba34c68bbdcc8bfe9ea
guillaumeArrigoni <guillaume.arr@gmail.com>
2022-11-10 18:44:16 +0100

    S3 - Fixing bug error and Update toString in Comparison

--- src/gamepoker/Comparison.java
+++ src/gamepoker/Comparison.java
@@ -5,23 +5,47 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+/**NOTE
+ * for the combination : color :
+ * for the test when both hand have color :
+ * reuse the chooseWinningSingle (just add the check if all the card have the same color)
+ */
 
 public class Comparison {
     /**
+     * THE WINNING COMBINATION
      * winningCombination give the number associate to the combination that is winning
      * The value are :
      * Equality : 0
-     * Single Card : 1
+     * Single : 1
      * Pair : 2
      * Double pair : 3
      * Brelan : 4
+     * Suite : 5
+     * Color : 6
+     * Full : 7
+     * Carre : 8
+     * Quinte Flush : 9
      * .....
+     *
+     * THE VALUE ASSOCIATE TO THE COMBINATION IN THE DICO
+     * Single : 1
+     * Pair : 2
+     * Brelan : 3
+     * Carre : 4
      */
     public static final int EQUALITY = 0;
-    public static final int SINGLE = 1;
-    public static final int PAIR = 2;
-    public static final int TWO_PAIR = 3;
-    public static final int BRELAN = 4;
+    public static final int SINGLE_FOR_DICO_KEY = 1;
+    public static final int PAIR_FOR_DICO_KEY = 2;
+    public static final int BRELAN_FOR_DICO_KEY = 3;
+
+    public static final int SINGLE_FOR_COMBINATION = 1;
+    public static final int PAIR_FOR_COMBINATION = 2;
+    public static final int TWO_PAIR_FOR_COMBINAISON = 3;
+    public static final int BRELAN_FOR_COMBINATION = 4;
+    public static final int COLOR = 6;
+
+
 
     private final HashMap<Integer, String> correspondanceCombinaisonEntierString = new HashMap<>();
     private int winningCombination;
@@ -43,17 +67,15 @@ public class Comparison {
         if (winningCombination != 0){
             texte = texte + " of ";
         }
-        /*Value value = new Value(winningValue.get(0));
+        Value value = new Value(winningValue.get(0));
         texte = texte + value.toString();
-         */
-        texte = texte + winningValue.get(0);
         return texte;
     }
 
     private void initDictionary(){
         correspondanceCombinaisonEntierString.put(1, "the highest card : ");
-        correspondanceCombinaisonEntierString.put(2, "a pair");
-        correspondanceCombinaisonEntierString.put(3, "a double pair");
+        correspondanceCombinaisonEntierString.put(2, "a PAIR_FOR_DICO_KEY");
+        correspondanceCombinaisonEntierString.put(3, "a double PAIR_FOR_DICO_KEY");
         correspondanceCombinaisonEntierString.put(4, "a three of a kind");
     }
     public Boolean getWinning() {
@@ -69,45 +91,50 @@ public class Comparison {
     }
 
     private boolean haveSingle(HandPoker hand) {
-        return hand.getHandOccurrence().containsValue(SINGLE);
+        return hand.getHandOccurrence().containsValue(SINGLE_FOR_DICO_KEY);
     }
 
     private boolean havePair(HandPoker hand) {
-        return hand.getHandOccurrence().containsValue(PAIR);
+        return hand.getHandOccurrence().containsValue(PAIR_FOR_DICO_KEY);
     }
 
     private boolean haveBrelan(HandPoker hand) {
-        return hand.getHandOccurrence().containsValue(BRELAN);
+        return hand.getHandOccurrence().containsValue(BRELAN_FOR_DICO_KEY);
     }
 
     private ArrayList<Integer> getSingle(HandPoker hand) {
-        return hand.getHandCombination().get(SINGLE);
+        return hand.getHandCombination().get(SINGLE_FOR_DICO_KEY);
     }
 
     private ArrayList<Integer> getPair(HandPoker hand) {
-        return hand.getHandCombination().get(PAIR);
+        return hand.getHandCombination().get(PAIR_FOR_DICO_KEY);
     }
 
     private Integer getBrelan(HandPoker hand) {
-        return hand.getHandCombination().get(BRELAN).get(0); /* only one brelan can be reach in any hand -> list of 1 element*/
+        return hand.getHandCombination().get(BRELAN_FOR_DICO_KEY).get(0); /* only one brelan can be reach in any hand -> list of 1 element*/
     }
 
     /**
      * return the number to the higher combination
      * Single card : 1
      * Pair : 2
-     * Two pairs : 3
+     * Double pair : 3
      * Brelan : 4
+     * Suite : 5
+     * Color : 6
+     * Full : 7
+     * Carre : 8
+     * Quinte Flush : 9
      *
      * @return the number of the higher combination
      */
     private int chooseCombination(HandPoker hand) {
         if (haveBrelan(hand)) {
-            return BRELAN;
+            return BRELAN_FOR_COMBINATION;
         } else if (havePair(hand)) {
-            return PAIR;
+            return PAIR_FOR_COMBINATION;
         } else {
-            return SINGLE;
+            return SINGLE_FOR_COMBINATION;
         }
     }
 
@@ -116,14 +143,21 @@ public class Comparison {
      */
     private Boolean chooseWinningHand() {
         if (chooseCombination(this.hand1) == chooseCombination(this.hand2)) {
-            if (chooseCombination(this.hand1) == PAIR) {
-                if (chooseWinningPair() == null) {
+            switch (chooseCombination(this.hand1)){
+                case BRELAN_FOR_COMBINATION :
+                    if (chooseWinningBrelan() == null) {
+                        return chooseWinningSingle(); /*car si la deuxième plus haute combinaison est une pair alors il s'agit d'un full et non un brelan*/
+                    } else {
+                        return chooseWinningBrelan();
+                    }
+                case PAIR_FOR_COMBINATION: case TWO_PAIR_FOR_COMBINAISON:
+                    if (chooseWinningPair() == null) {
+                        return chooseWinningSingle();
+                    } else {
+                        return chooseWinningPair();
+                    }
+                default: /*when case equals to SINGLE8FOR8COMBINATION*/
                     return chooseWinningSingle();
-                } else {
-                    return chooseWinningPair();
-                }
-            } else {
-                return chooseWinningSingle();
             }
         } else {
             return chooseWinningCombination();
@@ -137,8 +171,8 @@ public class Comparison {
      */
     private ArrayList<Integer> correspondenceValueComposition(int number, HandPoker hand) {
         ArrayList<Integer> listValue = new ArrayList<>();
-        if (number == BRELAN) {
-            listValue.add(getBrelan(hand));
+        if (number == BRELAN_FOR_COMBINATION) {
+            listValue.add(getBrelan(hand)); /* car getBrelan ne renvoie pas une liste */
             return listValue;
         } else {
             return getPair(hand);
@@ -166,7 +200,7 @@ public class Comparison {
     }
 
     /**
-     * return a Boolean to know the winning brelan between 2 brelans
+     * return a Boolean to know the winning brelan between 2 brelan
      * Update winningCombination and winningValue
      *
      * @return (true, false or null) if the first brelan is (higher, lower or equals) to the second brelan
@@ -174,11 +208,11 @@ public class Comparison {
     private Boolean chooseWinningBrelan() {
         if (getBrelan(this.hand1) > getBrelan(this.hand2)) {
             this.winningValue = new ArrayList<>(Collections.singletonList(getBrelan(this.hand1)));
-            this.winningCombination = BRELAN;
+            this.winningCombination = BRELAN_FOR_COMBINATION;
             return true;
         } else if (getBrelan(this.hand1) < getBrelan(this.hand2)) {
             this.winningValue = new ArrayList<>(Collections.singletonList(getBrelan(this.hand2)));
-            this.winningCombination = BRELAN;
+            this.winningCombination = BRELAN_FOR_DICO_KEY;
             return false;
         } else {
             return null;
@@ -186,25 +220,25 @@ public class Comparison {
     }
 
     /**
-     * return a Boolean to know the winning pair between 2 brelans
+     * return a Boolean to know the winning pair between 2 pairs
      * Update winningCombination and winningValue
      * @return (true, false or null) if the first pair is (higher, lower or equals) to the second pair
      */
     private Boolean chooseWinningPair() {
         ArrayList<Integer> thisPairSorted = getPair(this.hand1);
         ArrayList<Integer> handPairSorted = getPair(this.hand2);
-        thisPairSorted.sort(Collections.reverseOrder());
+        thisPairSorted.sort(Collections.reverseOrder()); /* trier par ordre decroissant pour comparer en premier les plus hautes paires des deux mains*/
         handPairSorted.sort(Collections.reverseOrder());
 
         for (int i = 0; i < thisPairSorted.size(); i++) {
             if (thisPairSorted.get(i) != handPairSorted.get(i)) {
                 if (thisPairSorted.get(i) > handPairSorted.get(i)) {
                     this.winningValue = new ArrayList<>(Collections.singletonList(thisPairSorted.get(i)));
-                    this.winningCombination = PAIR;
+                    this.winningCombination = PAIR_FOR_COMBINATION;
                     return true;
                 } else {
                     this.winningValue = new ArrayList<>(Collections.singletonList(handPairSorted.get(i)));
-                    this.winningCombination = PAIR;
+                    this.winningCombination = PAIR_FOR_COMBINATION;
                     return false;
                 }
             }
@@ -230,11 +264,11 @@ public class Comparison {
             if (thisSingleSorted.get(i) != handSingleSorted.get(i)) {
                 if (thisSingleSorted.get(i) > handSingleSorted.get(i)) {
                     this.winningValue = new ArrayList<>(Collections.singletonList(thisSingleSorted.get(i)));
-                    this.winningCombination = SINGLE;
+                    this.winningCombination = SINGLE_FOR_COMBINATION;
                     return true;
                 } else {
                     this.winningValue = new ArrayList<>(Collections.singletonList(handSingleSorted.get(i)));
-                    this.winningCombination = SINGLE;
+                    this.winningCombination = SINGLE_FOR_COMBINATION;
                     return false;
                 }
             }
