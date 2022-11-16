62359e6f84528944aed6f662cbbcfd0d1a69fe99
Karim CHARLEUX <karim.charleux@etu.univ-cotedazur.fr>
2022-11-16 22:09:12 +0100

    S2 - Create ComparisonTest & HandPokerTest (not finish)

--- Test/gamepoker/ComparisonTest.java
+++ Test/gamepoker/ComparisonTest.java
@@ -1,25 +1,23 @@
 package gamepoker;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ComparisonTest {
-    private static Card c1;
-    private static Card c2;
-    private static Card c3;
 
-    @BeforeAll
-    public static void setup() {
-        c1 = new Card(new Value("10"));
-        c2 = new Card(new Value("A"));
-        c3 = new Card(new Value("A"));
+    @Test
+    void getWinning() {
+        //TODO : Finish this
     }
 
     @Test
-    void testEquals() {
-        assertNull(c2.CompareHigherCard(c3));
-        assertTrue(c2.CompareHigherCard(c1));
-        assertFalse(c1.CompareHigherCard(c2));
+    void getWinningValue() {
+        //TODO : Finish this
     }
+
+    @Test
+    void getWinningCombination() {
+        //TODO : Finish this
+    }
+
+    //TODO : Finish test
 }
--- Test/gamepoker/HandPokerTest.java
+++ Test/gamepoker/HandPokerTest.java
@@ -1,7 +1,43 @@
 package gamepoker;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HandPokerTest {
 
+    public static HandPoker handASPoker;
+    private static Card cardAS = new Card("A");
+
+    @BeforeAll
+    static void setupHandPokerWithCards(){
+        handASPoker = new HandPoker(new ArrayList<>(Arrays.asList(cardAS,cardAS,cardAS,cardAS,cardAS)));
+    }
+
+    @Test
+    void getHandCardsMethodTest() {
+        assertEquals(HandPoker.NBR_CARDS,handASPoker.getHandCards().size());
+        assertTrue(handASPoker.getHandCards().containsAll(new ArrayList<>(Arrays.asList(cardAS,cardAS,cardAS,cardAS,cardAS))));
+    }
+
+    @Test
+    void getHandOccurrence() {
+        //TODO : Finish this
+    }
+
+    @Test
+    void getHandCombination() {
+        //TODO : Finish this
+    }
+
+    @Test
+    void testToString() {
+        //TODO : Finish this
+    }
+
+    //TODO : Finish test
 }
