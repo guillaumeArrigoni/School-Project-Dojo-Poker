8bf479f3c436d2b90da3cf5ec49d6a547a8cc5bb
Karim CHARLEUX <karim.charleux@etu.univ-cotedazur.fr>
2022-11-21 12:50:39 +0100

    S6 - Update HandPokerTest

--- Test/gamepoker/HandPokerTest.java
+++ Test/gamepoker/HandPokerTest.java
@@ -6,92 +6,62 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HandPokerTest {
 
-    public static HandPoker handASPoker;
-    public static HandPoker brelanCinq;
-    private static Card neufPi;
-    private static Card dixPi;
-    private static Card valetPi;
-    private static Card damePi;
-    private static Card roiPi;
-    private static Card sixCo;
-    private static Card septCo;
-    private static Card huitCo;
-    private static Card neufCo;
-    private static Card deuxPi;
-    private static Card quatrePi;
-    private static Card roiCo;
-    private static Card roiCa;
-    private static Card valetCo;
-    private static Card valetCa;
-    private static Card cinqTr;
-    private static Card cinqPi;
-    private static Card cinqCo;
-    private static Card huitPi;
-    private static Card huitTr;
-    private static Card huitCa;
-
-
-
-    private static HandPoker straightFlushCo;
-
-    private static HandPoker pairOfEight;
-    private static HandPoker carreOfEight;
-    public static Card cardAS;
+    private static HandPoker brelanCinq;
+    private static HandPoker quinteFlushCo;
+    private static HandPoker paireDeHuit;
+    private static HandPoker deuxPairesDeHuitEtDeCinq;
+    private static HandPoker carreDeHuit;
 
     @BeforeAll
     public static void setupComparison() throws PokerException {
-        cardAS = new Card("ATr");
-        cinqPi = new Card("5Pi");
-        cinqTr = new Card("5Tr");
-        deuxPi = new Card("2Pi");
-        quatrePi = new Card("4Pi");
-        neufPi = new Card("9Pi");
-        dixPi = new Card("10Pi");
-        valetPi = new Card("VPi");
-        damePi = new Card("DPi");
-        roiPi = new Card("RPi");
-        cinqCo = new Card("5Co");
-        sixCo = new Card("6Co");
-        septCo = new Card("7Co");
-        huitCo = new Card("8Co");
-        neufCo = new Card("9Co");
-        roiCo = new Card("RCo");
-        roiCa = new Card("RCa");
-        valetCo = new Card("VCo");
-        valetCa = new Card("VCa");
-        huitTr = new Card("8Tr");
-        huitCa = new Card("8Ca");
-        huitPi = new Card("8Pi");
+        Card quatrePi = new Card("4Pi");
+        Card cinqTr = new Card("5Tr");
+        Card cinqPi = new Card("5Pi");
+        Card cinqCo = new Card("5Co");
+        Card sixCo = new Card("6Co");
+        Card septCo = new Card("7Co");
+        Card huitCo = new Card("8Co");
+        Card huitTr = new Card("8Tr");
+        Card huitCa = new Card("8Ca");
+        Card huitPi = new Card("8Pi");
+        Card neufPi = new Card("9Pi");
+        Card neufCo = new Card("9Co");
 
-        handASPoker = new HandPoker(new ArrayList<>(Arrays.asList(cardAS, cardAS, cardAS, cardAS, cardAS)));
-        straightFlushCo = new HandPoker(new ArrayList<>(Arrays.asList(cinqCo, sixCo, septCo, huitCo, neufCo)));
+        quinteFlushCo = new HandPoker(new ArrayList<>(Arrays.asList(cinqCo, sixCo, septCo, huitCo, neufCo)));
         brelanCinq = new HandPoker(new ArrayList<>(Arrays.asList(cinqCo, cinqPi, cinqTr, neufPi, quatrePi)));
-        pairOfEight = new HandPoker(new ArrayList<>(Arrays.asList(huitCo, huitPi, cinqTr, neufPi, quatrePi)));
-        carreOfEight = new HandPoker(new ArrayList<>(Arrays.asList(huitCo, huitPi, huitTr, huitCa, quatrePi)));
-
+        paireDeHuit = new HandPoker(new ArrayList<>(Arrays.asList(huitCo, huitPi, cinqTr, neufPi, quatrePi)));
+        deuxPairesDeHuitEtDeCinq = new HandPoker(new ArrayList<>(Arrays.asList(huitCo, huitPi, cinqTr, cinqCo, quatrePi)));
+        carreDeHuit = new HandPoker(new ArrayList<>(Arrays.asList(huitCo, huitPi, huitTr, huitCa, quatrePi)));
     }
 
     @Test
-    void HandOccurenceTestWith() {
-        int testCinqCo = cinqCo.getValue().getPosition();
-        int testHuit = huitPi.getValue().getPosition();
-        assertEquals(straightFlushCo.getHandOccurrence().get(testCinqCo), 1);
-        assertEquals(pairOfEight.getHandOccurrence().get(testHuit), 2);
-        assertEquals(brelanCinq.getHandOccurrence().get(testCinqCo), 3);
-        assertEquals(carreOfEight.getHandOccurrence().get(testHuit),4);
+    void sameCardOccurrenceWithDifferentNumberOfCards() {
+        int fiveValue = Value.CINQ.getPosition();
+        int eightValue = Value.HUIT.getPosition();
+        assertEquals(1, quinteFlushCo.getHandOccurrence().get(fiveValue));
+        assertEquals(2, paireDeHuit.getHandOccurrence().get(eightValue));
+        assertEquals(3, brelanCinq.getHandOccurrence().get(fiveValue));
+        assertEquals(4, carreDeHuit.getHandOccurrence().get(eightValue));
     }
 
+    @Test
+    void checkIfEachCardIsDifferentTest() {
+        assertEquals(HandPoker.NBR_CARDS, quinteFlushCo.getHandOccurrence().size());
+    }
 
     @Test
-    void getHandCombination() {
-        assertEquals(brelanCinq.getHandCombination().get(Comparison.BRELAN_FOR_DICO_KEY).get(0), 5);
-        assertEquals(carreOfEight.getHandCombination().get(Comparison.CARRE_FOR_DICO_KEY).get(0), 8);
+    void checkDifferentHandCombination() {
+        assertEquals(8, paireDeHuit.getHandCombination().get(Comparison.PAIR_FOR_DICO_KEY).get(0));
+        List huitEtCinqList = new ArrayList<>(Arrays.asList(5, 8)); //List need to be sorted
+        assertEquals(huitEtCinqList, deuxPairesDeHuitEtDeCinq.getHandCombination().get(Comparison.PAIR_FOR_DICO_KEY));
+        assertEquals(5, brelanCinq.getHandCombination().get(Comparison.BRELAN_FOR_DICO_KEY).get(0));
+        assertEquals(8, carreDeHuit.getHandCombination().get(Comparison.CARRE_FOR_DICO_KEY).get(0));
     }
 }
 
